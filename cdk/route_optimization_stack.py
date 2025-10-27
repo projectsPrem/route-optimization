565edea15334f2f6021ac4367f9df3ea05e32287
Prem <premkumar@Prems-MacBook-Air.local>
2025-10-28 02:26:17 +0530

    add: ebs flask

--- cdk/route_optimization_stack.py
+++ cdk/route_optimization_stack.py
@@ -1,45 +1,50 @@
-import aws_cdk as cdk
 from aws_cdk import (
-    aws_s3 as s3,
+    Stack,
+    aws_s3_assets as s3_assets,
     aws_elasticbeanstalk as elasticbeanstalk,
+    aws_iam as iam,
 )
+from constructs import Construct
 
-class RouteOptimizationStack(cdk.Stack):
+class RouteOptimizationStack(Stack):
+    def __init__(self, scope: Construct, construct_id: str, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
 
-    def __init__(self, scope: cdk.App, id: str, **kwargs):
-        super().__init__(scope, id, **kwargs)
+        # Upload Flask app zip to S3
+        app_zip_asset = s3_assets.Asset(self, "FlaskAppZip", path="../app.zip")
 
-        # 🪣 Create an S3 bucket to hold app.zip
-        bucket = s3.Bucket(
-            self, "FlaskAppBucket",
-            versioned=False,
-            removal_policy=cdk.RemovalPolicy.DESTROY,
-            auto_delete_objects=True
-        )
-
-        # 🧠 Create Elastic Beanstalk Application
+        # Beanstalk Application
         app = elasticbeanstalk.CfnApplication(
-            self, "FlaskApplication",
-            application_name="route-optimization"
+            self, "FlaskApp", application_name="route-optimization-app"
         )
 
-        # 📦 Upload app.zip manually or through GitHub Actions before deploy
-        app_version = elasticbeanstalk.CfnApplicationVersion(
-            self, "AppVersion",
-            application_name=app.application_name,
-            source_bundle=elasticbeanstalk.CfnApplicationVersion.SourceBundleProperty(
-                s3_bucket=bucket.bucket_name,
-                s3_key="app.zip"
-            )
+        # IAM Role
+        role = iam.Role(
+            self,
+            "FlaskEBRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AWSElasticBeanstalkWebTier"),
+                iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3FullAccess"),
+            ],
         )
 
-        app_version.add_dependency(app)
+        # Use the correct Platform ARN for your region
+        platform_arn = "arn:aws:elasticbeanstalk:ap-south-1::platform/Python 3.9 running on 64bit Amazon Linux 2023/4.7.4"
 
-        # 🌍 Elastic Beanstalk Environment (Stable Python version)
-        env = elasticbeanstalk.CfnEnvironment(
-            self, "FlaskEnvironment",
+        # Environment
+        elasticbeanstalk.CfnEnvironment(
+            self,
+            "FlaskEnvironment",
             environment_name="route-optimization-env",
             application_name=app.application_name,
-            solution_stack_name="64bit Amazon Linux 2 v3.5.5 running Python 3.9",
-            version_label=app_version.ref
+            platform_arn=platform_arn,
+            version_label="v1",
+            option_settings=[
+                elasticbeanstalk.CfnEnvironment.OptionSettingProperty(
+                    namespace="aws:autoscaling:launchconfiguration",
+                    option_name="IamInstanceProfile",
+                    value="aws-elasticbeanstalk-ec2-role",
+                ),
+            ],
         )
